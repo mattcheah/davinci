cb6b63ba253f14d18ac5470bbdcca11ef53057ed
Matt Cheah <matt.cheah@gmail.com>
2017-08-17 17:24:52 +0000

    finish parse_sublime_xml to store all sublime info into the @options hash

--- models/syntax_information.rb
+++ models/syntax_information.rb
@@ -12,13 +12,48 @@ class SyntaxInformation
     
     def parse_sublime_xml
         #Parses all of the sublime xml document here into options and @from_editor_specific_information
-        @options['theme_name'] = /name<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
+        @options[:theme_name] = /name<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
+        @file = @file[@file.index(@options[:theme_name])..-1]
         
-        @options['background_color'] = /<key>settings<\/key>.*?<dict>.*?<key>background<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
-        @options['foreground_color'] = /<key>settings<\/key>.*?<dict>.*?<key>foreground<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
+        settings_colors = ["background", "foreground", "caret", "invisibles", "lineHighlight", "selection", "findHighlight", "findHighlightForeground", "selectionBorder", "activeGuide", "bracketsForeground", "bracketContentsForeground", ]
+        settings_style = ["bracketsOptions", "bracketContentsOptions", "tagsOptions"]
+        
+        settings_colors.each do |setting|
+            @options[setting.to_sym] = /<key>settings<\/key>.*?<dict>.*?<key>#{setting}<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
+        end 
+        
+        settings_style.each do |setting|
+            @options["#{setting}_style".to_sym] = /<key>settings<\/key>.*?<dict>.*?<key>#{setting}<\/key>.*?<string>(.*?)<\/string>/smx.match(@file)[1]
+        end
+        
+        @file.scan(/<dict>.*?<key>name<\/key>.*?<string>(.*?)<\/string>.*?<dict>.*?<key>foreground<\/key>.*?<string>(.*?)<\/string>/mx) do |m|
+            name = (m[0].underscore+"_foreground").to_sym
+            @options[name] = m[1]
+        end
+        
+        @file.scan(/<dict>.*?<key>name<\/key>.*?<string>(.*?)<\/string>.*?<dict>.*?<key>background<\/key>.*?<string>(.*?)<\/string>/mx) do |m|
+            name = (m[0].underscore+"_background").to_sym
+            @options[name] = m[1]
+        end
+        
+         @file.scan(/<dict>.*?<key>name<\/key>.*?<string>(.*?)<\/string>.*?<dict>.*?<key>fontStyle<\/key>.*?<string>(.*?)<\/string>/mx) do |m|
+            name = (m[0].underscore+"_font_style").to_sym
+            @options[name] = m[1]
+        end
+       
         puts @options
     end
     
     def parse_atom_xml
     end
+end
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-. ", "_").
+    downcase
+  end
 end
