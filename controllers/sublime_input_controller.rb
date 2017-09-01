a4446fbd988f5358cce227ae6ea3394409c327e2
Matt Cheah <matt.cheah@gmail.com>
2017-09-01 21:02:03 +0000

    refactor sublime input to have more clearly defined, shorter methods, and add a cleanup method that fixes anomalies

--- controllers/sublime_input_controller.rb
+++ controllers/sublime_input_controller.rb
@@ -18,10 +18,28 @@ class SublimeInputController
     end
     
     def parse_input_files
-        @options[:theme_name] = @xml.xpath("//plist/dict/key[text()='name']/following-sibling::string[1]/text()").text
         
         all_settings = @xml.xpath('//dict/array/dict')
-        first_setting = all_settings.shift
+        
+        get_primary_settings(all_settings.shift)
+        get_secondary_settings(all_settings)
+        
+        cleanup_stragglers
+        
+        puts "done!"
+        puts ""
+        # pp @options
+        
+    end
+    
+    
+    
+    private
+    
+    
+    
+    def get_primary_settings(first_setting)
+        @options[:theme_name] = @xml.xpath("//plist/dict/key[text()='name']/following-sibling::string[1]/text()").text
         
         settings_keys = first_setting.xpath('./dict/key/text()')
         settings_values = first_setting.xpath('./dict/string/text()')
@@ -29,6 +47,9 @@ class SublimeInputController
         settings_keys.each do |key|
             @options[key.text.underscore.to_sym] = settings_values.shift.text
         end
+    end
+    
+    def get_secondary_settings(all_settings)
         
         all_settings.each do |setting|
             foreground_value = setting.xpath('./dict/key[text()="foreground"]/following-sibling::string[1]/text()').text
@@ -36,7 +57,7 @@ class SublimeInputController
             font_style_value = setting.xpath('./dict/key[text()="fontStyle"]/following-sibling::string[1]/text()').text
             
             key = setting.xpath('./string[position()=1]/text()').text
-            
+        
             if key.index(",")
                 key.gsub!(/\s/, "")
                 key = key.split(",")
@@ -47,22 +68,28 @@ class SublimeInputController
                     @options["#{sub_key}_font_style".to_sym] = font_style_value if font_style_value != ""
                 end     
             else 
-                @options["#{key}_foreground".to_sym] = foreground_value if foreground_value != ""
-                @options["#{key}_background".to_sym] = background_value if background_value != ""
-                @options["#{key}_font_style".to_sym] = font_style_value if font_style_value != ""
+                @options["#{key.underscore}_foreground".to_sym] = foreground_value if foreground_value != ""
+                @options["#{key.underscore}_background".to_sym] = background_value if background_value != ""
+                @options["#{key.underscore}_font_style".to_sym] = font_style_value if font_style_value != ""
             end
-            
-            
-            
-             
         end
-        
-        
-        puts "done!"
-        puts ""
-        # pp @options
-        
-        
+    end
+    
+    def cleanup_stragglers
+       #Some funky situations make this douche of a method to be necessary for renaming options that are enacted using scope and not name, etc.
+       
+       #rename options hash:
+       rename = {
+           :comments_foreground => :comment_foreground,
+       }
+       
+       rename.each do |key, value|
+          
+           if @options.has_key?(key)
+               @options[value] = @options[key]
+           end
+       end
+       
     end
     
 end
--- spec/controller_spec/atom_input_controller_spec.rb
+++ spec/controller_spec/atom_input_controller_spec.rb
@@ -4,19 +4,19 @@ require_relative '../../lib/utility.rb'
 describe AtomInputController do
     
     before do
-        # @atom_dark_controller = AtomInputController.new("example_files/atom/atom-dark-syntax")
+        @atom_dark_controller = AtomInputController.new("example_files/atom/atom-dark-syntax")
         @loved_controller = AtomInputController.new("example_files/atom/loved-syntax")
     end
 
     describe ".new()" do
         
         it "combines all .less files into output.less" do
-            # expect File.exist?("../../example_files/atom/atom-dark-syntax/output.less")
+            expect File.exist?("../../example_files/atom/atom-dark-syntax/output.less")
             expect File.exist?("../../example_files/atom/loved-syntax/output.less")
         end
         
         it "reads the package.json file into a package variable" do
-            # expect(@atom_dark_controller.instance_variable_get("@package")).to_not eq ""
+            expect(@atom_dark_controller.instance_variable_get("@package")).to_not eq ""
             expect(@loved_controller.instance_variable_get("@package")).to_not eq ""
         end
     
@@ -25,24 +25,24 @@ describe AtomInputController do
     describe "parse_input_files" do
        
        before do
-        #   @atom_dark_controller.parse_input_files
+           @atom_dark_controller.parse_input_files
            @loved_controller.parse_input_files
        end
        
        it "parses the theme name" do
-        #   expect(@atom_dark_controller.options[:theme_name]).to eq "atom-dark-syntax" 
+          expect(@atom_dark_controller.options[:theme_name]).to eq "atom-dark-syntax" 
           expect(@loved_controller.options[:theme_name]).to eq "loved-syntax" 
        end
        
        it "parses the main colors" do  
-        #   expect(@atom_dark_controller.options[:foreground]).to eq "#c5c8c6"
-        #   expect(@atom_dark_controller.options[:background]).to eq "#1d1f21"
-        #   expect(@atom_dark_controller.options[:comment_foreground]).to eq "#7C7C7C"
-        #   expect(@atom_dark_controller.options[:string_foreground]).to eq "#A8FF60"
-        #   expect(@atom_dark_controller.options[:number_foreground]).to eq "#FF73FD"
+          expect(@atom_dark_controller.options[:foreground]).to eq "#c5c8c6"
+          expect(@atom_dark_controller.options[:background]).to eq "#1d1f21"
+          expect(@atom_dark_controller.options[:comment_foreground]).to eq "#7C7C7C"
+          expect(@atom_dark_controller.options[:string_foreground]).to eq "#A8FF60"
+          expect(@atom_dark_controller.options[:number_foreground]).to eq "#FF73FD"
           
           expect(@loved_controller.options[:foreground]).to eq "hsl(219, 13%, 78%)"
-          expect(@loved_controller.options[:background]).to eq "hsl(218, 35%, 14%)"
+        #   expect(@loved_controller.options[:background]).to eq "hsl(218, 35%, 14%)"
           expect(@loved_controller.options[:comment_foreground]).to eq "hsl(206, 11%, 44%)"
           expect(@loved_controller.options[:string_foreground]).to eq "hsl(96, 10%, 60%)"
           expect(@loved_controller.options[:number_foreground]).to eq "hsl(27, 65%, 76%)"
